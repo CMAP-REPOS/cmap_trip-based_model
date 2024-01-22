65c6dd6eb13e61a904c46162092239d8e4750aa9
Nick Ferguson <nrferguson@users.noreply.github.com>
2024-01-22 07:32:30 -0600

    build: move trip roster export out of subprocess

--- results/src/tbmtools/results/trip_roster.py
+++ results/src/tbmtools/results/trip_roster.py
@@ -1,8 +1,5 @@
-import subprocess
 from pathlib import Path
-from textwrap import dedent
 import pandas as pd
-from tbmtools import config
 
 def export(projdir, outdir):
     """
@@ -26,23 +23,11 @@ def export(projdir, outdir):
     hh_types = ['typical', 'wfh', 'deadhead']
     hh_type_trip_rosters = {}
     for t in hh_types:
-        jobfiles = pqdir.glob(f'*{t}.pq')
-        dfs = (pd.read_parquet(f).reset_index().set_index('purpose') for f in jobfiles)
+        jobfiles = sorted(pqdir.glob(f'*{t}.pq'))
+        dfs = [pd.read_parquet(f).reset_index().set_index('purpose') for f in jobfiles]
         hh_type_trip_rosters.update({t: pd.concat(dfs)})
-    complete_trip_roster = pd.concat(hh_type_trip_rosters, names=['hh_type'])
+    complete_trip_roster = pd.concat(hh_type_trip_rosters, names=['hh_type'], sort=False)
     outdir.mkdir(parents=True, exist_ok=True)
     trip_roster_path = outdir.joinpath('trip_roster.csv')
     complete_trip_roster.to_csv(trip_roster_path)
     return trip_roster_path
-
-def run_export(projdir, outdir):
-    """
-    Run trip_roster.export() in a custom environment to allow access to 
-    parquet engines.
-    """
-    
-    cli = subprocess.run(f'"{config.CONDAPATH}\\Scripts\\activate.bat" {config.ENVNAME} && python -m tbmtools.results.export_trip_roster {projdir} {outdir}',
-                         shell=True,
-                         capture_output=True,
-                         text=True)
-    return cli.stdout
